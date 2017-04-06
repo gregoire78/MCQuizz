916f47f5e78155edeffe656cc9a5919fa4794ea1
ITELIOS\gjoncour <gregoire.joncour@itelios.com>
2017-04-06 13:39:45 +0200

    update resto

--- app/controllers/Home.js
+++ app/controllers/Home.js
@@ -1,17 +1,21 @@
-var request = require('request');
+"use strict";
+import request from 'request';
 
-var Home = {
+const Home = {
     /**
      * @param req La requête entrante
      * @param res Ce qui est renvoyé au navigateur
      */
-    index: function(req, res){
-		request('http://api.gregoirejoncour.xyz/restaurant', function (error, response, body) {
-			console.log('error:', error); // Print the error if one occurred 
-			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-			console.log('body:', body); // Print the HTML for the Google homepage. 
-		})
-		res.render('index', { title: 'Express' });
+    index: (req, res) => {
+        let restaurants = {};
+        request.get('http://api.gregoirejoncour.xyz/restaurant', function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log(JSON.parse(body).restaurants);
+            restaurants = JSON.parse(body).restaurants;
+            res.render('index', {title: 'Express', restaurants: restaurants});
+        });
+
     }
 };
 
